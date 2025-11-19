19250ce8571bbbc7e9101e8c075a8dcf038ad354
DREAMMACHINE\tszkr <tsz.krol@gmail.com>
2025-11-19 18:43:34 +0100

    Dodano weryfikacje tworzenia konta poprzez kod SMS

--- app/src/main/java/com/coinlet/register/ConfirmCodeRegisterStep.kt
+++ app/src/main/java/com/coinlet/register/ConfirmCodeRegisterStep.kt
@@ -2,15 +2,39 @@ package com.coinlet.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.coinlet.R
 import com.coinlet.databinding.ActivityConfirmCodeRegisterStepBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 
 class ConfirmCodeRegisterStep : AppCompatActivity() {
     private lateinit var binding: ActivityConfirmCodeRegisterStepBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var btnConfirmCode: Button
+//    private lateinit var resendTV: TextView
+    private lateinit var code1: EditText
+    private lateinit var code2: EditText
+    private lateinit var code3: EditText
+    private lateinit var code4: EditText
+    private lateinit var code5: EditText
+    private lateinit var code6: EditText
+
+    private lateinit var OTP : String
+    private lateinit var resendToken : PhoneAuthProvider.ForceResendingToken
+    private lateinit var phoneNumber : String
+    private lateinit var nationality : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +48,120 @@ class ConfirmCodeRegisterStep : AppCompatActivity() {
         }
 
 
+        OTP = intent.getStringExtra("OTP").toString()
+        resendToken = intent.getParcelableExtra("resendToken")!!
+        phoneNumber = intent.getStringExtra("phoneNumber")!!
+        nationality = intent.getStringExtra("nationality")!!
 
+
+        init()
+
+        addTextChangeListener()
         binding.btnConfirmCode.setOnClickListener {
-            val nationality = intent.getStringExtra("nationality") ?: ""
-            val phoneNumber = intent.getStringExtra("phoneNumber") ?: ""
-            val intent = Intent(this, ThirdRegisterStep::class.java)
-            intent.putExtra("nationality", nationality)
-            intent.putExtra("phoneNumber", phoneNumber)
-            startActivity(intent)
+            val typedOTP = (code1.text.toString() + code2.text.toString() + code3.text.toString()
+                    + code4.text.toString() + code5.text.toString() + code6.text.toString())
+            if (typedOTP.isEmpty()) {
+                Toast.makeText(this, "Wprowadź kod", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (typedOTP.length != 6) {
+                Toast.makeText(this, "Wprowadź poprawny kod", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val credential: PhoneAuthCredential =
+                PhoneAuthProvider.getCredential(OTP, typedOTP)
+
+            verifyCode(credential)
+
+
+//        binding.btnConfirmCode.setOnClickListener {
+//            val nationality = intent.getStringExtra("nationality") ?: ""
+//            val phoneNumber = intent.getStringExtra("phoneNumber") ?: ""
+//            val intent = Intent(this, ThirdRegisterStep::class.java)
+//            intent.putExtra("nationality", nationality)
+//            intent.putExtra("phoneNumber", phoneNumber)
+//            startActivity(intent)
         }
     }
+
+    private fun addTextChangeListener(){
+        code1.addTextChangedListener(EditTextWatcher(code1))
+        code2.addTextChangedListener(EditTextWatcher(code2))
+        code3.addTextChangedListener(EditTextWatcher(code3))
+        code4.addTextChangedListener(EditTextWatcher(code4))
+        code5.addTextChangedListener(EditTextWatcher(code5))
+        code6.addTextChangedListener(EditTextWatcher(code6)) }
+    private fun init(){
+        auth = FirebaseAuth.getInstance()
+        btnConfirmCode = findViewById(R.id.btnConfirmCode)
+        code1 = binding.code1
+        code2 = binding.code2
+        code3 = binding.code3
+        code4 = binding.code4
+        code5 = binding.code5
+        code6 = binding.code6
+    } // resendCode = findViewById() code1 = findViewById(R.id.code1) code2 = findViewById(R.id.code2) code3 = findViewById(R.id.code3) code4 = findViewById(R.id.code4) code5 = findViewById(R.id.code5) code6 = findViewById(R.id.code6) }
+
+    inner class EditTextWatcher(private val view : View) : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val text = s.toString()
+                when(view.id){
+                    R.id.code1 -> if(text.length == 1) code2.requestFocus()
+                    R.id.code2 -> if(text.length == 1) code3.requestFocus() else if (text.isEmpty()) code1.requestFocus()
+                    R.id.code3 -> if(text.length == 1) code4.requestFocus() else if (text.isEmpty()) code2.requestFocus()
+                    R.id.code4 -> if(text.length == 1) code5.requestFocus() else if (text.isEmpty()) code3.requestFocus()
+                    R.id.code5 -> if(text.length == 1) code6.requestFocus() else if (text.isEmpty()) code4.requestFocus()
+                    R.id.code6 -> if(text.isEmpty()) code5.requestFocus() } }
+
+        override fun beforeTextChanged(
+            s: CharSequence?,
+            start: Int, count: Int,
+            after: Int ) {
+
+        }
+
+        override fun onTextChanged(
+            s: CharSequence?, start: Int,
+            before: Int,
+            count: Int ) {
+
+        }
+
+    }
+    private fun verifyCode(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Numer telefonu został zweryfikowany",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    val intent = Intent(this, ThirdRegisterStep::class.java).apply {
+                        putExtra("nationality", nationality)
+                        putExtra("phoneNumber", phoneNumber)
+                    }
+                    startActivity(intent)
+                    finish()
+                } else {
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(
+                            this,
+                            "Nieprawidłowy kod weryfikacyjny",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Błąd weryfikacji: ${task.exception?.localizedMessage}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/coinlet/register/SecondStepRegister.kt
+++ app/src/main/java/com/coinlet/register/SecondStepRegister.kt
@@ -5,21 +5,44 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.coinlet.R
 import com.coinlet.databinding.ActivitySecondStepRegisterBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class SecondStepRegister : AppCompatActivity() {
     private lateinit var binding: ActivitySecondStepRegisterBinding
+    private lateinit var btnSendCode : Button
+    private lateinit var phoneNumberInputText : EditText
+    private lateinit var auth : FirebaseAuth
+    private lateinit var phoneNumber : String
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivitySecondStepRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -52,15 +75,111 @@ class SecondStepRegister : AppCompatActivity() {
         }
 
 
-        binding.btnSendCode.setOnClickListener {
+        init()
+
+        btnSendCode.setOnClickListener{
+
+            phoneNumber = phoneNumberInputText.text.trim().toString()
+            if (phoneNumber.isNotEmpty()){
+                if(phoneNumber.length in 7..12){
+                    val selected = binding.numberSpinner.selectedItem.toString()
+                    val parts = selected.split(" ")
+                    val numberPrefix = parts.last()
+                    phoneNumber = numberPrefix + phoneNumber
+
+                    val options = PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber(phoneNumber) // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this) // Activity (for callback binding)
+                        .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+                        .build()
+                    PhoneAuthProvider.verifyPhoneNumber(options)
+
+                }else{
+                    Toast.makeText(this, "Podaj właściwy numer telefonu!", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Podaj numer telefonu!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+//        binding.btnSendCode.setOnClickListener {
+//            val nationality = intent.getStringExtra("nationality") ?: ""
+//            val phoneNumber = binding.phoneNumberInputText.text.toString()
+//            val intent = Intent(this, ConfirmCodeRegisterStep::class.java)
+//            intent.putExtra("nationality", nationality)
+//            intent.putExtra("phoneNumber", phoneNumber)
+//            startActivity(intent)
+//        }
+
+
+    }
+    private fun init(){
+        btnSendCode = findViewById(R.id.btnSendCode)
+        auth = FirebaseAuth.getInstance()
+        phoneNumberInputText = binding.phoneNumberInputText
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            } else if (e is FirebaseAuthMissingActivityForRecaptchaException) {
+                // reCAPTCHA verification attempted with null Activity
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken,
+        ) {
             val nationality = intent.getStringExtra("nationality") ?: ""
-            val phoneNumber = binding.phoneNumberInputText.text.toString()
-            val intent = Intent(this, ConfirmCodeRegisterStep::class.java)
+            val intent = Intent(this@SecondStepRegister, ConfirmCodeRegisterStep::class.java)
+            intent.putExtra("OTP", verificationId)
+            intent.putExtra("resendToken", token)
             intent.putExtra("nationality", nationality)
             intent.putExtra("phoneNumber", phoneNumber)
             startActivity(intent)
         }
-
-
     }
+
+
+
+    //funkcja dla logowania
+//    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+//        auth.signInWithCredential(credential)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//
+//                    val user = task.result?.user
+//                } else {
+//                    // Sign in failed, display a message and update the UI
+//                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+//                        // The verification code entered was invalid
+//                    }
+//                    // Update UI
+//                }
+//            }
+//    }
+
+
 }
